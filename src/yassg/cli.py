ca4231e8ccb293ea67b16a2b1867ef0945ef5ae8
lordxarus <lordxarus@gmail.com>
2025-09-09 23:35:37 -0400

    refactor: use importlib.resources for default template

--- src/yassg/cli.py
+++ src/yassg/cli.py
@@ -6,39 +6,43 @@ import argparse
 import logging
 
 import sys
+import importlib.resources
 
 logger = logging.getLogger(__name__)
 
 
 def main():
 
-    log_level = logging.DEBUG
-    logging.basicConfig(
-        level=log_level,
-        format="%(asctime)s - %(name)s:%(lineno)d - %(levelname)s: %(message)s",
-    )
+    with importlib.resources.as_file(
+        (importlib.resources.files("yassg") / "template.html")
+    ) as default_tmpl_path:
+        log_level = logging.DEBUG
+        logging.basicConfig(
+            level=log_level,
+            format="%(asctime)s - %(name)s:%(lineno)d - %(levelname)s: %(message)s",
+        )
 
-    parser = argparse.ArgumentParser(
-        prog="yassg",
-        description="Create a static website from simple markdown\
-                         and static files",
-    )
+        parser = argparse.ArgumentParser(
+            prog="yassg",
+            description="Create a static website from simple markdown\
+                             and static files",
+        )
 
-    parser.add_argument("source_dir")
-    parser.add_argument("--output_dir", default=Path("public"), required=False)
-    parser.add_argument("--tmpl_path", default=Path("template.html"), required=False)
+        parser.add_argument("source_dir")
+        parser.add_argument("--output_dir", default=Path("public"), required=False)
+        parser.add_argument("--template", default=default_tmpl_path, required=False)
 
-    args = parser.parse_args()
+        args = parser.parse_args()
 
-    tmpl_path = Path(args.tmpl_path)
-    in_dir = Path(args.source_dir)
-    out_dir = Path(args.output_dir)
+        tmpl_path = Path(args.tmpl_path)
+        in_dir = Path(args.source_dir)
+        out_dir = Path(args.output_dir)
 
-    try:
-        build(in_dir, out_dir, tmpl_path)
-    except (OSError, FileNotFoundError, PermissionError) as e:
-        logger.critical(f"failed to build {e}")
-        sys.exit(-1)
+        try:
+            build(in_dir, out_dir, tmpl_path)
+        except (OSError, FileNotFoundError, PermissionError) as e:
+            logger.critical(f"failed to build {e}")
+            sys.exit(-1)
 
 
 if __name__ == "__main__":
